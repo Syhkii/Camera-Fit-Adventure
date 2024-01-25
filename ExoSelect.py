fb70ebbe7b7356d65f5517c35937de5af1921674
Adri5gamer <154530224+Adri5gamer@users.noreply.github.com>
2024-01-25 17:36:00 +0100

    Update ExoSelect.py

--- ExoSelect.py
+++ ExoSelect.py
@@ -12,34 +12,33 @@ muscle_des_avants_bras = True
 for x in range(len(str(data).split('\n'))):
     if 'Deltoïde' in str(data_sep[x]) and Deltoide:
         exo = str(data_sep[x]).split(';')
-
         if str(exo[5]) == "Pas de matérielle":
             if str(exo[1,3]) not in str(listexo):
-                listexo.append(exo[1])
-    if 'Biceps' in str(data_sep[x]) and Deltoide:
+                listexo.append(exo[1,3])
+    
+    if 'Biceps' in str(data_sep[x]) and Biceps:
         exo = str(data_sep[x]).split(';')
-
         if str(exo[5]) == "Pas de matérielle":
             if str(exo[1,3]) not in str(listexo):
-                listexo.append(exo[1])
-    if 'Biceps_Brachial' in str(data_sep[x]) and Deltoide:
+                listexo.append(exo[1,3])
+    
+    if 'Brachial' in str(data_sep[x]) and Brachial:
         exo = str(data_sep[x]).split(';')
-
         if str(exo[5]) == "Pas de matérielle":
             if str(exo[1,3]) not in str(listexo):
-                listexo.append(exo[1])
-    if 'Biceps_Brachial' in str(data_sep[x]) and Deltoide:
+                listexo.append(exo[1,3])
+    
+    if 'Biceps_Brachial' in str(data_sep[x]) and Biceps_Brachial:
         exo = str(data_sep[x]).split(';')
-
         if str(exo[5]) == "Pas de matérielle":
             if str(exo[1,3]) not in str(listexo):
-                listexo.append(exo[1])
-    if 'muscle_des_avants_bras' in str(data_sep[x]) and Deltoide:
+                listexo.append(exo[1,3])
+                
+    if 'muscle_des_avants_bras' in str(data_sep[x]) and muscle_des_avants_bras:
         exo = str(data_sep[x]).split(';')
-
         if str(exo[5]) == "Pas de matérielle":
             if str(exo[1,3]) not in str(listexo):
-                listexo.append(exo[1])
+                listexo.append(exo[1,3])
 
 
 print(listexo)
